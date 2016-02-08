c00178ee59977a055c0054ca82cd36713afdce05
David Stichter <david.stichter3414@gmail.com>
2016-02-08 18:59:53 -0500

    Code Working

--- server.js
+++ server.js
@@ -1,2 +1,39 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+
+var PORT = 8090;
+
+app.get("/addition/:var1/:var2", function(req, res) {
+  var x = parseInt(req.params.var1)
+  var y = parseInt(req.params.var2)
+
+  var response = x + y
+  res.sendStatus(response)
+
+});
+app.get("/subtract/:var1/:var2", function(req, res) {
+  var x = parseInt(req.params.var1)
+  var y = parseInt(req.params.var2)
+
+  var response = x - y
+  res.sendStatus(response)
+});
+app.get("/multiply/:var1/:var2", function(req, res) {
+  var x = parseInt(req.params.var1)
+  var y = parseInt(req.params.var2)
+
+  var response = x * y
+  res.sendStatus(response)
+});
+app.get("/divide/:var1/:var2", function(req, res) {
+  var x = parseInt(req.params.var1)
+  var y = parseInt(req.params.var2)
+
+  var response = x / y
+  res.sendStatus(response)
+});
+
+
+app.listen(PORT, function() {
+  console.log("Listening on port %s", PORT);
+});
